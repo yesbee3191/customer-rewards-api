16f52552506b38ab36e260e27707f42436f83ee3
Prem Sagar <c-premsagar.bathini@charter.com>
2020-05-02 01:30:46 -0500

    stream logic

--- src/main/java/com/charter/customerrewards/builder/CustomerrewardsBuiilder.java
+++ src/main/java/com/charter/customerrewards/builder/CustomerrewardsBuiilder.java
@@ -15,8 +15,10 @@ public class CustomerrewardsBuiilder {
 
     public List<CustomerRewards> customerInfoBuild(List<Customer> customers, Set<String> cusNames){
 
-
-
+      CustomerRewards customerRewardsList =
+                customerInfoBuildHelper(customers.stream()
+                       .filter(customer -> cusNames.stream()
+                               .anyMatch(cusName -> customer.getName().equalsIgnoreCase("berlin"))).collect(Collectors.toList()));
         return null;
     }
 
